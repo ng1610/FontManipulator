479a8e04edb9390187aca1329a632c9dcbbf9ef7
ng1610 <74397928+ng1610@users.noreply.github.com>
2022-01-29 16:28:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+leftWristX="";
+rightWristX="";
+difference="";
+
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(480, 500);
+    canvas=createCanvas(420, 420);
+    canvas.position(500, 100);
+
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX= results[0].poses.leftWrist.x;
+        rightWristX= results[0].poses.rightWrist.x;
+        difference=floor(leftWristX - rightWristX);
+    }
+}
+function draw(){
+    textSize(difference);
+    fill("#FF0000");
+    text('Navya', 50, 400);
+}
